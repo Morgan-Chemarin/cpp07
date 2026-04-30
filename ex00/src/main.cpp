c7f35dc97d89f111510a8c7a612b24e6deaf4f8a
Morgan Chemarin <mchemari@z3r11p2.42lyon.fr>
2026-05-01 00:12:25 +0200

    main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mchemari <mchemari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 23:40:45 by mchemari          #+#    #+#             */
-/*   Updated: 2026/05/01 00:03:37 by mchemari         ###   ########.fr       */
+/*   Updated: 2026/05/01 00:11:15 by mchemari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,18 @@ int main()
     std::cout << "Min of " << a << " and " << b;
     std::cout << ":\n | "<< ::min(a, b) << std::endl;
     std::cout << "Max of " << a << " and " << b;
-    std::cout << ":\n | "<< ::max(a, b) << std::endl;
+    std::cout << ":\n | "<< ::max(a, b) << std::endl << std::endl;
+
+    std::cout << "-----------------------------------------------\n" << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    
+    std::cout << "Before swap: \n | c = " << c << "\n | d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "After swap: \n | c = " << c << "\n | d = " << d << std::endl << std::endl;
+    std::cout << "Min of " << c << " and " << d;
+    std::cout << ":\n | "<< ::min(c, d) << std::endl;
+    std::cout << "Max of " << c << " and " << d;
+    std::cout << ":\n | "<< ::max(c, d) << std::endl << std::endl;
 }
